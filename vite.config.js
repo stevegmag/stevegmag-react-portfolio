c754dfc79eb4c9afaa4a256258ceffd05112020e
Steven Gallagher <stevegmag@gmail.com>
2025-04-01 17:43:40 -0400

    update ci/cd to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
-});
+  base: '/stevegmag-react-portfolio/', // Replace with your repository name
+})
