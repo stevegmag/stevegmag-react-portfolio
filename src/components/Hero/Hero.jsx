4ba87d77cc622e9752029b2f097cd9f267af1583
Steven Gallagher <stevegmag@gmail.com>
2025-04-01 18:23:33 -0400

    updated hero image and hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,18 +7,17 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Steven</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a full-stack developer with years of experience building sites and applications with Javascript (ES6+) | Modern JS Frameworks | PHP | Python | Node | Drupal | Wordpress
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="mailto:stevegmag@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
-        alt="Hero image of me"
+        src={getImageUrl("hero/SteveG-bigcopy-alpha.png")}
+        alt="Hero image of Steven"
         className={styles.heroImg}
       />
       <div className={styles.topBlur} />
