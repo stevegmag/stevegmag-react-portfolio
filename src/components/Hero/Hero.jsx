9aa6e67c915b071164832f2cdd48c00db3eb2653
Steven Gallagher <stevegmag@gmail.com>
2025-04-02 13:51:48 -0400

    Udated Memojis, Comapny Logos, Skills logos, Updated Hero, About, Experience content components, Updated default colors

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,14 +9,14 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Steven</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with years of experience building sites and applications with Javascript (ES6+) | Modern JS Frameworks | PHP | Python | Node | Drupal | Wordpress
+        I'm a full-stack developer with years of experience building sites and applications using: JavaScript (ES6+) | Modern JS Frameworks | Node.js | PHP | Python | Drupal | WordPress
         </p>
         <a href="mailto:stevegmag@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/SteveG-bigcopy-alpha.png")}
+        src={getImageUrl("steveg/SteveG-Hi.webp")}
         alt="Hero image of Steven"
         className={styles.heroImg}
       />
