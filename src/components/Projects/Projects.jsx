0e5352f54a0698825f3131b31d4756a203f190c6
Steven Gallagher <stevegmag@gmail.com>
2025-04-02 19:45:14 -0400

    updated memojis in different sections

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,10 +4,17 @@ import styles from "./Projects.module.css";
 
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
+import { getImageUrl } from "../../utils";
+
 
 export const Projects = () => {
   return (
     <section className={styles.container} id="projects">
+      <img
+        src={getImageUrl("steveg/SteveG-Perfect.webp")}
+        alt="Steven Perfect Projects image"
+        className={styles.projectsImage}
+      />
       <h2 className={styles.title}>Projects</h2>
       <div className={styles.projects}>
         {projects.map((project, id) => {
