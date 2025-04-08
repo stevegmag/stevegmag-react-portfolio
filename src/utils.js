003390e4f7bf094a64cdee056296c3545158a96b
Steven Gallagher <stevegmag@gmail.com>
2025-04-08 19:36:24 -0400

    pdf fix v10

--- src/utils.js
+++ src/utils.js
@@ -5,9 +5,8 @@ export const getImageUrl = (path) => {
 export const getFileUrl = (path) => {
   const isProduction = import.meta.env.PROD;
   const baseUrl = isProduction ? '/stevegmag-react-portfolio' : '';
-  // Remove 'assets/' from the path for PDFs
   console.log(`Resolving URL for ${path} in ${isProduction ? 'production' : 'development'}`);
-  const finalUrl = `${baseUrl}/${path}`;
+  const finalUrl = `${baseUrl}/files/${path}`;
   console.log('Final URL:', finalUrl);
   return finalUrl;
 };
